45bead3604324c8119956e8ba1aba9aaecdcff32
unknown <medhanie.desale@powercoders.org>
2020-04-14 12:34:36 +0200

    DataFrame

---
+++ Plotly-Dashboard-with-Dash-med/Barcharts/exam1.py
@@ -0,0 +1,18 @@
+import plotly.offline as pyo
+import plotly.graph_objs as go
+import pandas as pd
+
+df = pd.read_csv(
+    r"C:\Users\User\Desktop\My_projects\Python_for_Data_science\Plotly-Dashboard-with-Dash-med\SourceData\2018WinterOlympics.csv")
+print(df)
+trace1 = go.Bar(x=df["NOC"], y=df["Gold"], name="Gold",
+                marker={"color": "#FFD700"})
+trace2 = go.Bar(x=df["NOC"], y=df["Silver"], name="Silver",
+                marker={"color": "#9EA0A1"})
+trace3 = go.Bar(x=df["NOC"], y=df["Bronze"], name="Bronze",
+                marker={"color": "#CD7F32"})
+
+data = [trace1, trace2, trace3]
+layout = go.Layout(title="Medals", barmode="stack")
+fig = go.Figure(data=data, layout=layout)
+pyo.plot(fig, filename="bar1.html")
